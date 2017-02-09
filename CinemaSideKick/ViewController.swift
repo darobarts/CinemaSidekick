5965bdb38b82834b51a040f33e4825ebfd3860b7
Santi Angelo Pierini <admin@pcp077663pcs.wireless.calpoly.edu>
2017-02-08 19:13:10 -0800

    Added the information stack. working on scroll stack

--- CinemaSideKick/ViewController.swift
+++ CinemaSideKick/ViewController.swift
@@ -9,16 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var ratingControl: RatingControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
 
 }
