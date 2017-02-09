359747e06011982222713e7a3037985e160c2523
Santi Angelo Pierini <admin@pcp077663pcs.wireless.calpoly.edu>
2017-02-08 17:35:34 -0800

    Initial Commit

---
+++ CinemaSideKick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CinemaSideKick
+//
+//  Created by Santi Angelo Pierini on 2/8/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
