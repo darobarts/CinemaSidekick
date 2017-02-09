cff63f3e0b62083371a69848d92cc3c219d6292a
Austin Robarts <austinrobarts1@gmail.com>
2017-02-09 12:25:34 -0800

    Fixed project file to show movieGetter

--- CinemaSideKick/MovieGetter.swift
+++ CinemaSideKick/MovieGetter.swift
@@ -9,29 +9,29 @@ import Foundation
 
 
 class MovieGetter {
-   
-   
+    
+    
     func getMovie(movieId: String, completion: @escaping (NSDictionary)->()) {
-      let url = URL(string: "https://api.themoviedb.org/" + movieId + "/movie/11?api_key=91702af3d9c57566bf5167a404863b64")
-      print("Start")
-      
-      let task = URLSession.shared.dataTask(with: url!) {
-        (data, response, err) in
-         guard err == nil else {
-            print(err ?? "No error found")
-            return
-         }
-         guard let data = data else {
-            print("Data is empty")
-            return
-         }
-         
-         let json = try! JSONSerialization.jsonObject(with: data, options: [])
-        completion(json as! NSDictionary)
-      }
-      task.resume()
-      
-   }
+        let url = URL(string: "https://api.themoviedb.org/" + movieId + "/movie/11?api_key=91702af3d9c57566bf5167a404863b64")
+        print("Start")
+        
+        let task = URLSession.shared.dataTask(with: url!) {
+            (data, response, err) in
+            guard err == nil else {
+                print(err ?? "No error found")
+                return
+            }
+            guard let data = data else {
+                print("Data is empty")
+                return
+            }
+            
+            let json = try! JSONSerialization.jsonObject(with: data, options: [])
+            completion(json as! NSDictionary)
+        }
+        task.resume()
+        
+    }
     
     func getConfiguration(completion: @escaping (NSDictionary)->()) {
         let url = URL(string : "https://api.themoviedb.org/3/configuration?api_key=91702af3d9c57566bf5167a404863b64")
