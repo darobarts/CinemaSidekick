d3358cc8dc152e69c0980eda0b69a03a02668c55
Austin Robarts <austinrobarts1@gmail.com>
2017-02-09 12:22:34 -0800

    Got poster to show on screen

--- CinemaSideKick/MovieGetter.swift
+++ CinemaSideKick/MovieGetter.swift
@@ -5,20 +5,20 @@
 //  Created by Austin Robarts on 2/8/17.
 //
 //
-
 import Foundation
 
 
 class MovieGetter {
    
    
-   func getMovie(movieId: String) {
-      let url = URL(string: "https://api.themoviedb.org/3/movie/11?api_key=91702af3d9c57566bf5167a404863b64")
+    func getMovie(movieId: String, completion: @escaping (NSDictionary)->()) {
+      let url = URL(string: "https://api.themoviedb.org/" + movieId + "/movie/11?api_key=91702af3d9c57566bf5167a404863b64")
       print("Start")
       
-      let task = URLSession.shared.dataTask(with: url!) {data, response, err in
+      let task = URLSession.shared.dataTask(with: url!) {
+        (data, response, err) in
          guard err == nil else {
-            print(err)
+            print(err ?? "No error found")
             return
          }
          guard let data = data else {
@@ -27,9 +27,54 @@ class MovieGetter {
          }
          
          let json = try! JSONSerialization.jsonObject(with: data, options: [])
-         print(json)
+        completion(json as! NSDictionary)
       }
       task.resume()
       
    }
+    
+    func getConfiguration(completion: @escaping (NSDictionary)->()) {
+        let url = URL(string : "https://api.themoviedb.org/3/configuration?api_key=91702af3d9c57566bf5167a404863b64")
+        let task = URLSession.shared.dataTask(with: url!) {
+            (data, response, err) in
+            guard err == nil else {
+                print(err ?? "No error found")
+                return
+            }
+            guard let data = data else {
+                print("Data is empty")
+                return
+            }
+            
+            let json = try! JSONSerialization.jsonObject(with: data, options: [])
+            completion(json as! NSDictionary)
+        }
+        task.resume()
+    }
+    
+    func getPosterPath(dict : NSDictionary, poster_name : String) -> String {
+        let dict2 = dict.value(forKey: "images") as! NSDictionary
+        let baseUrl = dict2.value(forKey : "secure_base_url") as! String
+        let sizeArray = dict2.value(forKey: "poster_sizes") as! NSArray
+        let size = sizeArray.lastObject as! String
+        return baseUrl + size + poster_name
+    }
+    
+    func getPoster(posterPath : String, completion : @escaping (_ data : Data)->()) {
+        let url = URL(string : posterPath)
+        
+        let task = URLSession.shared.dataTask(with: url!) {
+            (data, response, err) in
+            guard err == nil else {
+                print(err ?? "No error found")
+                return
+            }
+            guard let data = data else {
+                print("Data is empty")
+                return
+            }
+            completion(data)
+        }
+        task.resume()
+    }
 }
--- CinemaSideKick/ViewController.swift
+++ CinemaSideKick/ViewController.swift
@@ -12,21 +12,42 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var ratingControl: RatingControl!
     @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var synopsis: UILabel!
+    @IBOutlet weak var releaseDate: UILabel!
+    @IBOutlet weak var moviePoster: UIImageView!
+    
 
     @IBAction func nextMovie(_ sender: UIButton) {
         movieTitle.text = "Default"
+        let movieGetter = MovieGetter()
+        movieGetter.getMovie(movieId: "3", completion : {(json : NSDictionary)->() in  self.setMovieInfo(dict: json)})
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-      let moviePicker = MovieGetter()
-      moviePicker.getMovie(movieId: "1")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//        // Dispose of any resources that can be recreated.
-//    }
+    func setMovieInfo(dict : NSDictionary) {
+        print("Setting movie info")
+        let movieGetter = MovieGetter()
+        let posterPath = dict.value(forKey: "poster_path") as! String?
+        movieGetter.getConfiguration(completion: {(json : NSDictionary)->()
+            in movieGetter.getPoster(posterPath: movieGetter.getPosterPath(dict: json, poster_name: posterPath!) ,completion: {(data : Data)->()
+                in DispatchQueue.main.async {
+                    self.moviePoster.image = UIImage(data : data) }
+                })})
+        
+        print(dict)
+        DispatchQueue.main.async {
+            self.movieTitle.text = dict.value(forKey: "title") as! String?
+            self.synopsis.text = dict.value(forKey: "overview") as! String?
+            self.releaseDate.text = dict.value(forKey: "release_date") as! String?
+            //TODO: set all other information here
+        }
+
+
+    }
 
 
 }
